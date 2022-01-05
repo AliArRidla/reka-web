0955bb4520cf418119bfe022887a9394a472cd5b
aliarridla <beeunik0@gmail.com>
2022-01-05 08:19:55 +0700

    add API mesins

---
+++ app/Http/Controllers/API/MesinController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Mesin;
+use Illuminate\Http\Request;
+
+class MesinController extends Controller
+{
+    //get all data
+    public function all(Request $request){
+        $id = $request->input('id');
+        $nama_mesin = $request->input('nama_mesin');
+        $type_mesin = $request->input('type_mesin ');
+        $id = $request->input('id');
+
+        // kondisi nya
+        if($id){
+            $mesin = Mesin::find($id);
+            if($mesin){
+                return ResponseFormatter::success(
+                    $mesin,'Data mesin berhasil diambil'
+                );
+            }else{
+                return ResponseFormatter::error(
+                    null,'Data mesin tidak berhasil diambil',404 
+                );
+            }
+        }
+    }
+}
