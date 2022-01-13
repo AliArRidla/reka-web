3ede1fdd6faf0ada268f6a864ceced50ad274376
aliarridla <beeunik0@gmail.com>
2022-01-13 07:47:56 +0700

    mengubah ui dashboard default

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <section class="section">
+        <div class="section-header">
+            <h1>Dashboard</h1>
         </div>
-    </div>
-</div>
+        <div class="container">
+            <h2>welcome to my paradise</h2>
+        </div>
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            {{ __('You are logged in!') }}
+        </div>
+    </section>
 @endsection
