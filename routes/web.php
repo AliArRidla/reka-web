2afc4d93025f6552034588962fb4ff05318860e2
aliarridla <beeunik0@gmail.com>
2022-01-14 11:12:04 +0700

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,11 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 //  ini adalah route mesin 
