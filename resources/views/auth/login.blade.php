52595845b7948cd207bbce0dc5ea83094e9bed3c
pradipta16 <gantangsanra@gmail.com>
2022-01-14 16:33:32 +0700

    add profile menu at navbar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,12 @@
                 <div class="row">
                     <div class="col-15 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand">
-                            <img src="../assets/img/reka-full.png" alt="logo" width="150"
-                                >
+                            <img src="../assets/img/reka-full.png" alt="logo" width="150">
                         </div>
                         <div class="card card-primary">
-                            <div class="card-header"><h4><b>Login</b></h4></div>
+                            <div class="card-header">
+                                <h4><b>Login</b></h4>
+                            </div>
                             <div class="card-body">
                                 <form method="POST" action="{{ route('login') }}">
                                     @csrf
@@ -81,6 +82,7 @@
                         </div>
                     </div>
                 </div>
+            </div>
         </section>
     </div>
 @endsection
