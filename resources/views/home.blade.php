d4c7bd1136c49f4261e24cac60e916db0a51c346
pradipta16 <gantangsanra@gmail.com>
2022-01-24 08:34:19 +0700

    add csrf token when login and register

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,7 @@
             <h1>Dashboard</h1>
         </div>
         <div class="section-header">
-            <h1>JANCOK</h1>
-        </div>
-        <div class="card-body">
-            @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-            @endif
-            
-            {{ __('You are logged in!') }}
-        </div>
-        <div class="container">
-            <h2>Ini halaman setelah login</h2>
+            <h1>Assalamu'alaikum Cantik</h1>
         </div>
     </section>
 @endsection
