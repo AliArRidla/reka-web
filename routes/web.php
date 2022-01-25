1b9c0321f0587fb1e1df2d2fe1abd6f568a0ba8c
pradipta16 <gantangsanra@gmail.com>
2022-01-25 15:04:17 +0700

    atur posisi container login

--- routes/web.php
+++ routes/web.php
@@ -26,52 +26,14 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-//  ini adalah route mesin 
-// Route::get('/machines/add_mesin', function () {
-//     return view('machines/add_mesin');
-// });
-
-// Route::get('/machines/list_mesin', function () {
-//     return view('machines/list_mesin');
-// });
-
-// Route::get('/machines/edit_mesin', function () {
-//     return view('machines/edit_mesin');
-// });
-
 Route::resource('machines', MesinController::class);
 Route::resource('users', UserController::class);
 Route::resource('parameters', ParameterController::class);
-
-// route users
-// Route::get('/users/create', function () {
-//     return view('users/create');
-// });
-
-// Route::get('/users/list_users', function () {
-//     return view('users/list_users');
-// });
-
-// Route::get('/users/edit_users', function () {
-//     return view('users/edit_users');
-// });
 
 // route history 
 Route::get('/history/history', function () {
     return view('/history/history');
 });
-
-// //route profile menu
-// Route::get('/profile/view_profile', function () {
-//     return view('/profile/view_profile');
-// });
-
-// Route::get('/profile/edit_profile', function () {
-//     return view('/profile/edit_profile');
-// });
 
 Auth::routes();
 
